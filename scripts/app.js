327f7b5315db8a6b59da114d0b9447be87f9212e
laurihy <lauri.hynynen@gmail.com>
2013-04-22 18:21:05 -0700

    sidebar and stuff

--- scripts/app.js
+++ scripts/app.js
@@ -4,55 +4,76 @@
 
 // converts titles into slugs
 App.generateSlug = function(title){
-	return title.replace(/\s/g, '-')
+    return title.replace(/\s/g, '-')
 }
 
 App.getFirstParagraph = function(post){
-	return $('<div>'+marked(post.content)+'</div>').find('p:first').text();
+    return $('<div>'+marked(post.content)+'</div>').find('p:first').text();
 }
 
 App.prettyDate = function(datestr){
-	return new Date(datestr).toString('MMMM d, yyyy')
+    return new Date(datestr).toString('MMMM d, yyyy')
+}
+
+App.renderTemplate = function(templatename, target, data){
+    var template = _.template($(templatename).html(), data);
+    $(target).html(template);
+}
+
+App.findPostBySlug = function(slug){
+    var post = _.find(App.posts, function(post){
+        return slug==App.generateSlug(post.title); 
+    });
+    return post;
+}
+
+App.findOffsetPost = function(post, offset){
+    var index = App.posts.indexOf(post);
+    if(index<0){
+        return false;
+    }
+    var newindex = index+offset;
+    if(newindex<0 || newindex>=App.posts.length){
+        return false;
+    }
+    return App.posts[newindex]
 }
 
 App.router = Backbone.Router.extend({
-	routes: {
-		"": "mainview",
-		"all": "all",
-		":slug": "post"
-	},
+    routes: {
+        "": "mainview",
+        ":slug": "post"
+    },
 
-	mainview: function(){
-		postcount = Math.min(App.posts.length, 4)
-		if(postcount>0){
-			var template = _.template($('#mainview').html(), {posts: App.posts, postcount: postcount});
-			$('#main').html(template);
-		} else {
-			template = _.template($('#error').html(),{error:'No posts (yet?)'});
-			$('#main').html(template);
-		}
-	},
+    mainview: function(){
+        var postcount = Math.min(App.posts.length, 5);
+        var data = {
+            post: undefined,
+            posts: App.posts,
+            postcount: postcount
+        }
+        App.renderTemplate('#mainview', '#main', data);
+        App.renderTemplate('#sidebar-template', '#sidebar', data);
 
-	all: function(){
-		if(App.posts.length>0){
-			var template = _.template($('#all').html(), {posts: App.posts});
-			$('#main').html(template);
-		} else {
-			template = _.template($('#error').html(),{error:'No posts (yet?)'});
-			$('#main').html(template);
-		}
-	},
+    },
 
-	post: function(slug){
-		var post = _.find(App.posts, function(post) { return slug==App.generateSlug(post.title) });
-		if(post!=undefined){
-			template = _.template($('#post').html(), {post: post});
-			$('#main').html(template);
-		} else {
-			template = _.template($('#error').html(),{error:'Post was not found'});
-			$('#main').html(template);
-		}
-	}
+    post: function(slug){
+        var post = App.findPostBySlug(slug)
+        var prev = App.findOffsetPost(post, +1);
+        var next = App.findOffsetPost(post, -1);
+
+        if(post!=undefined){
+            App.renderTemplate('#post','#main',{post:post, prev:prev, next:next});
+        } else {
+            App.renderTemplate('#error','#main',{error: 'Post was not found'});
+        }
+
+        App.renderTemplate('#sidebar-template','#sidebar',{
+                post: post,
+                posts: App.posts,
+                postcount: Math.min(App.posts.length, 5)
+        });
+    }
 
 })
 
